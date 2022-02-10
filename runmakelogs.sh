7d0b4c556ae36d093d1bc99859f80bbd5b83ce81
gwest <gwest@gwest@redhat.com-mac>
2022-02-10 17:40:55 -0600

    Add token in initial test

--- runmakelogs.sh
+++ runmakelogs.sh
@@ -1,4 +1,5 @@
 export VERBOSE_LOG="true"
+export token=`cat /etc/secret-volume/token`
 curl --insecure -H "Authorization: Bearer ${token}" https://elasticsearch.openshift-logging:9200
 /app/bin/makelogs --verbose --trace --host https://elasticsearch.openshift-logging:9200
 while true; do sleep 10000; done
